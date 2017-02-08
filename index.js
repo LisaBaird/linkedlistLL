5ef46483017276eee81aea695b19319d05a72f3b
Leta Keane <letakeane+turing@gmail.com>
2017-02-08 15:16:01 -0700

    Complete phase one requirements

--- index.js
+++ index.js
@@ -1,40 +1,26 @@
+//When user types in fields
+
+
+//When user clicks ENTER button
 function hideExample() {
   $('.example').addClass('hide');
 };
 
-//When user clicks ENTER button
 $('#btn-enter').on('click', function() {
     var inputTitle = $('#input-title').val();
     var inputURL = $('#input-url').val();
     $('#card-side').prepend("<section class='card'><h2 id='card-title' class='card-text'>" + inputTitle + "</h2><hr><p class='card-text'><a href='#' id='card-url'>" + inputURL + "</a></p><hr><button class='card-btn btn-read'>Read</button><button class='card-btn btn-remove'>Delete</button></section>");
     hideExample();
-    //BIND to new event only (not all)
-    $('.btn-read:first').on('click', function() {
-      markRead();
-    });
-    $('.btn-remove:first').on('click', function () {
-      clickDelete();
-    });
-    console.log($('.btn-read:first'));
-    console.log($('.btn-remove:first'));
 })
 
-//When user clicks READ button
-function markRead() {
-  $('.card').toggleClass('read');
-  $('#card-url').toggleClass('read');
-  $('.btn-read').toggleClass('read');
-};
-
-$('.btn-read').on('click', function() {
-  markRead();
+//When user clicks READ button on card
+$('#card-side').on('click', '.btn-read', function() {
+    $(this).parent().toggleClass('read');
+    $(this).siblings('.card-text').children('#card-url').toggleClass('read');
+    $(this).toggleClass('read');
 });
 
-//WHen user clicks DELETE button
-function clickDelete() {
-  $('.card').addClass('hide');
-};
-
-$('.btn-remove').on('click', function () {
-  clickDelete();
+//When user clicks DELETE button on card
+$('#card-side').on('click', '.btn-remove', function() {
+  $(this).parent().remove();
 });
