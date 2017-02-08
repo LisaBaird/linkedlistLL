934593ec0cfd02b8eb21bd5789c7f512302785f0
Leta Keane <letakeane+turing@gmail.com>
2017-02-07 19:51:25 -0700

    Add addClase to button click to remove the example card from view

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+function hideExample () {
+  $('.example').addClass('hide');
+};
+
 $('#btn-enter').on('click', function() {
     var inputTitle = $('#input-title').val();
     var inputURL = $('#input-url').val();
     $('#card-side').prepend("<section class='card'><h2 id='card-title' class='card-text'>" + inputTitle + "</h2><hr><p class='card-text'><a href= '#' id='card-url'>" + inputURL + "</a></p><hr><button id='btn-read' class='card-btn'>Read</button><button id='btn-remove' class='card-btn'>Delete</button></section>");
+    hideExample();
 })
