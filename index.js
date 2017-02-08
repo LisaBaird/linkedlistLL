9be484b8eaa23eb32c1b2984d305b899c24bd58f
Leta Keane <letakeane+turing@gmail.com>
2017-02-07 22:44:24 -0700

    Re-bind events

--- index.js
+++ index.js
@@ -2,20 +2,39 @@ function hideExample() {
   $('.example').addClass('hide');
 };
 
+//When user clicks ENTER button
 $('#btn-enter').on('click', function() {
     var inputTitle = $('#input-title').val();
     var inputURL = $('#input-url').val();
-    $('#card-side').prepend("<section class='card'><h2 id='card-title' class='card-text'>" + inputTitle + "</h2><hr><p class='card-text'><a href= '#' id='card-url'>" + inputURL + "</a></p><hr><button id='btn-read' class='card-btn'>Read</button><button id='btn-remove' class='card-btn'>Delete</button></section>");
+    $('#card-side').prepend("<section class='card'><h2 id='card-title' class='card-text'>" + inputTitle + "</h2><hr><p class='card-text'><a href='#' id='card-url'>" + inputURL + "</a></p><hr><button class='card-btn btn-read'>Read</button><button class='card-btn btn-remove'>Delete</button></section>");
     hideExample();
+    //BIND to new event only (not all)
+    $('.btn-read:first').on('click', function() {
+      markRead();
+    });
+    $('.btn-remove:first').on('click', function () {
+      clickDelete();
+    });
+    console.log($('.btn-read:first'));
+    console.log($('.btn-remove:first'));
 })
 
-$('#btn-read').on('click', function (event) {
+//When user clicks READ button
+function markRead(event) {
   $('.card').toggleClass('read');
   $('#card-url').toggleClass('read');
-  $('#btn-read').toggleClass('read');
+  $('.btn-read').toggleClass('read');
+};
+
+$('.btn-read').on('click', function() {
+  markRead();
 });
 
-$('#btn-remove').on('click', function () {
-  console.log("You clicked delete!");
+//WHen user clicks DELETE button
+function clickDelete(event) {
   $('.card').addClass('hide');
+};
+
+$('.btn-remove').on('click', function () {
+  clickDelete();
 });
