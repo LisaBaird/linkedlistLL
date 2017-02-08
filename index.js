258c5bd056020953c8ba7266a76a0a9ee324ab4b
Leta Keane <letakeane+turing@gmail.com>
2017-02-07 20:05:44 -0700

    Add addClass of read when read button is clicked

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ $('#btn-enter').on('click', function() {
     $('#card-side').prepend("<section class='card'><h2 id='card-title' class='card-text'>" + inputTitle + "</h2><hr><p class='card-text'><a href= '#' id='card-url'>" + inputURL + "</a></p><hr><button id='btn-read' class='card-btn'>Read</button><button id='btn-remove' class='card-btn'>Delete</button></section>");
     hideExample();
 })
+
+$('#btn-read').on('click', function () {
+  $('.card').toggleClass('read');
+  $('#card-url').toggleClass('read');
+  $('#btn-read').toggleClass('read');
+});
