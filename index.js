39fdf6141f865b58b646b25325205a87b5481f05
Leta Keane <letakeane+turing@gmail.com>
2017-02-08 17:08:10 -0700

    Complete functionality for phase-one and phase-two

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 //When user types in fields
+function enableBtn() {
+  if ($('#input-title').val().length >= 1 && $('#input-url').val().length >= 1) {
+    console.log('True');
+    $('#btn-enter').prop('disabled', false);
+  } else {
+    console.log('False');
+    $('#btn-enter').prop('disabled', true);
+  }
+}
+
+var inputs = $('input');
+
+for (var i = 0; i < inputs.length; i++) {
+  $(inputs[i]).on('keyup', function () {
+    enableBtn();
+  });
+}
 
 
 //When user clicks ENTER button
@@ -9,7 +26,7 @@ function hideExample() {
 $('#btn-enter').on('click', function() {
     var inputTitle = $('#input-title').val();
     var inputURL = $('#input-url').val();
-    $('#card-side').prepend("<section class='card'><h2 id='card-title' class='card-text'>" + inputTitle + "</h2><hr><p class='card-text'><a href='#' id='card-url'>" + inputURL + "</a></p><hr><button class='card-btn btn-read'>Read</button><button class='card-btn btn-remove'>Delete</button></section>");
+    $('#card-side').prepend("<section class='card'><h2 id='card-title' class='card-text'>" + inputTitle + "</h2><hr><p class='card-text'><a href='http://" + inputURL + "' id='card-url'>" + inputURL + "</a></p><hr><button class='card-btn btn-read'>Read</button><button class='card-btn btn-remove'>Delete</button></section>");
     hideExample();
 })
 
